d01380e7fcc6a7d7a66b9a8e116c9fe2e108e094
Kezzzzz <kieran.parish1805@gmail.com>
2020-08-06 14:57:06 +0800

    Imported fonts and setup modular css

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import UserList from '@components/UserList'
 
+import '@assets/css/default.scss'
+
 const root = document.getElementById('root')
 
 ReactDOM.render(<UserList />, root)
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,104 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ip = require("ip")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ip = require('ip')
 
 const resolve = require('path').resolve
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/app',
-    },
-    output: {
-        filename: 'js/bundle.js',
-        path: resolve('dist'),
-        publicPath: '/',
-    },
-    devServer: {
-        host: ip.address(),
-        port: 3200,
-        open: true,
-        historyApiFallback: true,
-        stats: 'errors-only',
-        contentBase: resolve('dist'),
-    },
-    module: { 
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    {
-                        loader: 'eslint-loader',
-                        options: {
-                            failOnWarning: false,
-                            failOnError: false,
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@root': resolve(),
-            '@components': resolve('src/components'),
-            '@hooks': resolve('src/hooks')
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            favicon: './public/favicon.ico'
-        })
+  mode: 'development',
+  entry: {
+    app: './src/app',
+  },
+  output: {
+    filename: 'js/bundle.js',
+    path: resolve('dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    host: ip.address(),
+    port: 3200,
+    open: true,
+    historyApiFallback: true,
+    stats: 'errors-only',
+    contentBase: resolve('dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              failOnWarning: false,
+              failOnError: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        oneOf: [
+          {
+            test: /\.module\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: { modules: true },
+              },
+              'sass-loader',
+            ],
+          },
+          {
+            use: [
+              //Creates `style` nodes from JS strings
+              'style-loader',
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'string-replace-loader',
+                options: {
+                  search: '/src/assets/fonts',
+                  replace: 'fonts',
+                  flags: 'g',
+                },
+              },
+              //Translate CSS into CommonJS
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 2,
+                },
+              },
+              'postcss-loader',
+              'sass-loader',
+            ],
+          },
+        ],
+      },
     ],
-    stats: 'errors-warnings',
-};
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@root': resolve(),
+      '@components': resolve('src/components'),
+      '@hooks': resolve('src/hooks'),
+      '@assets': resolve('src/assets'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+  ],
+  stats: 'errors-warnings',
+}
